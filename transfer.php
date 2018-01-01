60ae90d86988e5c03e5f58034cee4694e66e46c6
spyflying <nowherespyfly@outlook.com>
2018-01-01 23:37:03 +0800

    Update transfer.php

--- transfer.php
+++ transfer.php
@@ -1,26 +1,29 @@
 <?php
-//每次只能提现10元，实在写不下去了+_+
-require_once "public.php";
+//查询今天完成订单最多的用户
+require_once 'public.php';
 
-$uid = $_GET["uid"];
+$areaname = $_POST["street"];
 
-$uid_query = "SELECT *
-              FROM account
-              WHERE userid = '{$uid}';";
-$result = mysqli_query($db, $uid_query);
-while($row = mysqli_fetch_array($result)){
-	if($row["balance"] <= 110){
-		header("refresh:3;url = login.html"); 
-    	print("余额不足，无法进行转出，三秒后跳转到登录页面。。。。。。");
-	}
-	else{
-		$ubal = $row["balance"];
-	}
+$mysql_query = "CREATE VIEW userarea AS(SELECT userid FROM record WHERE oriadd = '{$areaname}');";
+//echo $mysql_query;
+
+$result = mysqli_query($db, $mysql_query);
+if(!$result){
+	die('Cannot create view: ' . mysqli_error($db));
 }
-$uid_query = "UPDATE account 
-              SET balance = balance - 10
-              WHERE userid = '{$uid}';";
-$result = mysqli_query($db, $uid_query);
-header("refresh:3;url = login.html"); 
-print("成功提现10元，三秒后跳转到登录页面。。。。");
+
+$mysql_query = "SELECT record.userid, sum(bill) AS totalmoney FROM record, userarea WHERE record.userid = userarea.userid GROUP BY userid;";
+$result = mysqli_query($db, $mysql_query);
+$arra = "";
+
+while($row = mysqli_fetch_array($result)){
+	//echo $row["userid"];
+	$arra = $arra . $row["userid"] . ", " . (string)$row["totalmoney"] . "; ";
+	//echo $arra;
+}
+
+echo $arra;
+
+$mysql_query = "DROP VIEW userarea;";
+$result = mysqli_query($db, $mysql_query);
 ?>
